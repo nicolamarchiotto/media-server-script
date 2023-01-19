b0cef13aa85e781c76941e48171e0c021cb4dd0e
nicolamarchiotto <ziapo98@gmail.com>
2023-01-19 21:32:12 +0100

    fixed jackett script

--- jackett.sh
+++ jackett.sh
@@ -4,9 +4,8 @@ sudo docker run -d \
   -e PGID=1000 \
   -e TZ=Europe/Rome \
   -e AUTO_UPDATE=true `#optional` \
-  -e RUN_OPTS=<run options here> `#optional` \
   -p 9117:9117 \
-  -v /jackett:/config \
-  -v /media:/downloads \
+  -v ~/jackett:/config \
+  -v ~/media:/downloads \
   --restart unless-stopped \
   lscr.io/linuxserver/jackett:latest
