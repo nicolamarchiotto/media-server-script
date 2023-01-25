f3390a2a2070560d051f09f3ea74adb1800262eb
nicolamarchiotto <ziapo98@gmail.com>
2023-01-25 20:32:01 +0100

    edit filebrowser

--- filebrowser.sh
+++ filebrowser.sh
@@ -1,1 +1,7 @@
-sudo docker run -d --name filebrowser -p 8081:8080 hurlenko/filebrowser
+sudo docker run -d \
+    --name filebrowser \
+    -p 8081:8080 \
+    -v /home/altair:/data \
+    -v /home/altair/filebrowser:/config \
+    --restart unless-stopped \
+    hurlenko/filebrowser
