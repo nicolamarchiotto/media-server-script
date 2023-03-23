b7f3e37ab235720ad18f35bb8f2268b79ef5e094
nicolamarchiotto <ziapo98@gmail.com>
2023-03-23 13:22:14 +0100

    fixed photoprism.sh

--- subscripts/photoprism.sh
+++ subscripts/photoprism.sh
@@ -3,9 +3,9 @@ sudo docker run -d \
   --security-opt seccomp=unconfined \
   --security-opt apparmor=unconfined \
   -p 2342:2342 \
-  -e PHOTOPRISM_UPLOAD_NSFW="true" \0
+  -e PHOTOPRISM_UPLOAD_NSFW="true" \
   -e PHOTOPRISM_ADMIN_PASSWORD="insecure" \
   -v /photoprism/storage \
   -v ~/Pictures:/photoprism/originals \
-  --restart unless-stopped 
-  photoprism/photoprism
+  --restart unless-stopped \
+  photoprism/photoprism:latest
