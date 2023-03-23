5a3888b23bb06b7e84c647d5faccf844457839d9
nicolamarchiotto <ziapo98@gmail.com>
2023-03-23 13:26:06 +0100

    fixed install

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo server install script
 ./subscripts/radar.sh
 ./subscripts/jackett.sh
 ./subscripts/filebrowser.sh
-./photoprism.sh
+./subscripts/photoprism.sh
 
 sudo ./subscripts/program_shutdown.sh
 
